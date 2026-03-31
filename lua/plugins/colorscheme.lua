0516d9a6a4f6554974c129f399fc179be18af08a
Vinney Cavallo <vinney@opennode.com>
2026-03-31 18:10:17 -0400

    Initial commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  { "vcavallo/grb256" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "grb256",
+      -- colorscheme = "quanganhdo/grb256",
+      -- colorscheme = "zanloy/vim-colors-grb256",
+    },
+  },
+}
