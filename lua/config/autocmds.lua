30f810432ddc9c3c5bca4876142b8a69964c63fb
Vinney Cavallo <vinney@opennode.com>
2026-03-31 18:24:56 -0400

    Make markdown tolerable by default

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,20 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function(args)
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+    vim.opt_local.textwidth = 80
+    vim.opt_local.formatoptions:remove("t")
+    vim.opt_local.formatoptions:remove("c")
+    vim.opt_local.formatexpr = "" -- clear this so gq uses textwidth
+    vim.schedule(function()
+      vim.cmd("RenderMarkdown disable") -- disable "rendering" by default
+    end)
+    vim.diagnostic.enable(false, { bufnr = args.buf }) -- same for diagnostics
+    vim.opt_local.spell = false
+  end,
+})
